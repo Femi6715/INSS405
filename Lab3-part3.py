e37de791a9c19b3aae91f6f9751d1c6ce3d81e85
Femi6715 <alaoo1109@students.bowiestate.edu>
2023-03-15 17:04:33 -0400

    Labwork

--- Lab3-part3.py
+++ Lab3-part3.py
@@ -1,7 +1,7 @@
 num=input('Enter input number')
 print(num)
-sum= int(num)
-if int(sum % 2)==0:
+test= int(num)
+if int(test % 2)==0:
     print('This is a even number')
 else:
     print('This is a odd number')
